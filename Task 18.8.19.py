05e5384967755201b47f85d9d4f608b281efeb1f
keysaryn <78592955+keysaryn@users.noreply.github.com>
2023-07-25 21:03:21 +0700

    Sp-Task_2 Добавлено: ввод возраста, переменные стоимости

--- Task 18.8.19.py
+++ Task 18.8.19.py
@@ -1,1 +1,16 @@
 amount_ticket = int(input("Введите количество билетов которые хотите приобрести: "))
+
+enter_age = int(input("Введите возраст: "))
+
+yang_price = 990
+full_price = 1390
+
+if amount_ticket > 1:
+
+
+# if amount_ticket > 3:
+#     print("Вам доступна 10% скидка")
+#     sum_order = sum_order * 90 / 100
+#     print("Сумма с учетом скидки: ")
+
+
